295658110636baa4688f90c7cac611a8e647b9f6
Ziga Setar <ziga.setar@agenda.si>
2023-05-03 13:25:08 +0200

    changes

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-
-
 from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
 from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
 from langchain.chat_models import ChatOpenAI
-
 from langchain.chains.question_answering import load_qa_chain
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
-
-
-OPENAI_API_KEY = ''
-PINECONE_API_KEY = ''
-PINECONE_API_ENV = ''
-
-
-
+OPENAI_API_KEY = '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'
+PINECONE_API_KEY = '235f7741-d74c-4f99-ac70-d4a99bbc2e30'
+PINECONE_API_ENV = 'us-west1-gcp-free'
 
 loader = UnstructuredPDFLoader('./field-guide-to-data-science.pdf')
 data = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
 texts = text_splitter.split_documents(data)
-
-
 
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY, model='text-embedding-ada-002')
 
@@ -39,31 +30,29 @@ docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, ind
 
 llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.3, openai_api_key=OPENAI_API_KEY)
 
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
-
-
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 rqa = ConversationalRetrievalChain.from_llm(llm, docsearch.as_retriever(), memory=memory)
-
-
-
 
 def retrieve_answer(query, chat_history):
     # retrieval_result = rqa({"question": query, "chat_history": chat_history})
     memory.chat_memory.add_user_message(query)
     
-
     res = rqa({"question": query})
     retrieval_result = res["answer"]
 
-    if "The given context does not provide" in retrieval_result or "I'm sorry" in retrieval_result or "context provided does not" in retrieval_result or "the given context does not provide" in retrieval_result or "The provided context does not contain" in retrieval_result or "I don't know" in retrieval_result or "This information is not provided" in retrieval_result or "in the given context" in retrieval_result:
-        print(query)
-        print([query])
-
-        base_result = llm.generate([query])
-        return base_result.generations[0][0].text
+    error_strings = ["The given context does not provide", "I'm sorry", "context provided does not",
+                    "the given context does not provide", "The provided context does not contain",
+                    "I don't know", "This information is not provided", "in the given context"]
+    
+    if any(error in retrieval_result for error in error_strings):    
+        print(type(query))
+        base_result = llm.generate(query)
+        return base_result
+        # return base_result.generations[0][0].text
     else:
+        """ sources = res["sources"]
+        source_info = f" (source: {sources[0]})" if sources else ""
+        return retrieval_result + source_info """
         return retrieval_result
 
 
@@ -75,8 +64,7 @@ def retrieve_answer(query, chat_history):
         for i, match in enumerate(matches):
             print(f"{i+1}. {match['metadata']['title']}: {match['metadata']['url']}") """
 
-
-# Start the chat loop
+# Chat loop
 messages = []
 
 print("Welcome to the chatbot. Enter 'quit' to exit the program.")
@@ -86,7 +74,6 @@ while True:
     if user_message.lower() == "quit":
         break
 
-    # Retrieve the answer to the user's question
     answer = retrieve_answer(user_message, messages)
     """  source_documents = answer['source_documents']
     print(source_documents) """
@@ -96,6 +83,3 @@ while True:
 
     """ if len(memory.chat_memory) > 5:
         memory.chat_memory.pop(0) """
-
-
-           
