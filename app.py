c2f47793d2846675c1860c74deff2c652a05062c
Ziga Setar <ziga.setar@agenda.si>
2023-05-03 09:32:03 +0200

    changes

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ from langchain.chat_models import ChatOpenAI
 
 from langchain.chains.question_answering import load_qa_chain
 
-import logging
-# logging.basicConfig(level=logging.DEBUG)
+
 
 OPENAI_API_KEY = ''
 PINECONE_API_KEY = ''
 PINECONE_API_ENV = ''
+
+
 
 
 loader = UnstructuredPDFLoader('./field-guide-to-data-science.pdf')
@@ -32,13 +33,13 @@ pinecone.init(
     api_key=PINECONE_API_KEY,
     environment=PINECONE_API_ENV
 )
-index_name = "chatconnect1"
+index_name = "test1"
 
 docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
 
 llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.3, openai_api_key=OPENAI_API_KEY)
 
-from langchain.memory import ConversationBufferMemory, ConversationSummaryMemory
+from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
 
@@ -51,11 +52,15 @@ rqa = ConversationalRetrievalChain.from_llm(llm, docsearch.as_retriever(), memor
 def retrieve_answer(query, chat_history):
     # retrieval_result = rqa({"question": query, "chat_history": chat_history})
     memory.chat_memory.add_user_message(query)
+    
 
     res = rqa({"question": query})
     retrieval_result = res["answer"]
 
     if "The given context does not provide" in retrieval_result or "I'm sorry" in retrieval_result or "context provided does not" in retrieval_result or "the given context does not provide" in retrieval_result or "The provided context does not contain" in retrieval_result or "I don't know" in retrieval_result or "This information is not provided" in retrieval_result or "in the given context" in retrieval_result:
+        print(query)
+        print([query])
+
         base_result = llm.generate([query])
         return base_result.generations[0][0].text
     else:
@@ -83,7 +88,14 @@ while True:
 
     # Retrieve the answer to the user's question
     answer = retrieve_answer(user_message, messages)
+    """  source_documents = answer['source_documents']
+    print(source_documents) """
     print("Assistant:", answer)
-
+    memory.chat_memory.add_ai_message(answer)
     messages.append((user_message, answer))
 
+    """ if len(memory.chat_memory) > 5:
+        memory.chat_memory.pop(0) """
+
+
+           
